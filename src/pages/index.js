79da220a69927a14b89802007bcfd8d574aa8fe9
DawitBezu <dawit.edsy@gmail.com>
2020-11-01 19:42:06 +0100

    edit cart

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export const query = graphql`
      img:file(relativePath:{eq:"default-background.jpg"}){
       childImageSharp{
         fluid{
-          ...GatsbyImageSharpFluid_tracedSVG
+          ...GatsbyImageSharpFluid
         }
       }
     } 
@@ -42,7 +42,7 @@ export const query = graphql`
           category
           image {
             fixed(width: 65, height: 65) {
-              ...GatsbyContentfulFixed_tracedSVG
+              ...GatsbyContentfulFixed
             }
           }
         }
