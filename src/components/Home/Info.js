97d9fa6450afadd7abcbd80a7b19d0b8d2f439ff
DawitBezu <dawit.edsy@gmail.com>
2020-11-01 19:56:36 +0100

    added hot coffee to about

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import { Link } from "gatsby"
 import Title from "../Globals/Title"
+import Coffee from '../../images/coffee.gif'
 
 const Info = () => {
   return ( 
-    <section className="py-5">
+    <section>
       <div className="container">
+        <div className="col py-5">
         <Title title="our shop"></Title>
-        <div className="row">
-          <div className="col-10 col-sm-8 mx-auto text-center">
-            <p className="lead text-muted mb-5">
-              
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, dicta deleniti. Est aliquam soluta explicabo, at distinctio repudiandae quam eum debitis ipsam dolorem minus, nostrum perferendis quos quae voluptas facere vitae architecto. Aspernatur, distinctio eveniet est unde sequi culpa nostrum eum soluta a repellat quidem, tempore aliquam ipsam ullam vel?
+          <div className="row">
+          <div className="col-10 col-sm-8 mx-auto">
+            <p className="lead text-muted">
+              we offer exotic coffee from several different regions of the world, and we take pride in our ability to roast current-crop, specialty-roasted gourmet coffee. These coffees are grown and produced using environmentally friendly practices, and they're also available in decaf. Each of our coffees is packaged in one-pound or five-pound bags, and we provide same-day roasting and shipping to ensure your gourmet coffee is fresh, flavorful and savory.
             </p>
             <Link to="/about/">
-                <button className="btn btn-yellow text-uppercase">about page</button>
+                <button className="btn btn-yellow text-center text-uppercase">about page</button>
             </Link>
-          </div>
+            </div>
+            <div className="aside">
+              <img src={Coffee} alt="coffee"/>
+            </div>
         </div>
+        </div>
+        
       </div>
     </section>
    );
