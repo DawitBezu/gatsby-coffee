8b866c0fc557e37b258e7409e2425d4f4e5164b9
DawitBezu <dawit.edsy@gmail.com>
2020-11-01 08:14:46 +0100

    added plugin to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN
       },
     }, 
     {
@@ -41,7 +41,7 @@ module.exports = {
     {
         resolve: 'gatsby-plugin-snipcart',
         options: {
-          apiKey: process.env.SNIPCART_APIKEY,
+          apiKey: process.env.GATSBY_SNIPCART_APIKEY,
           autopop: true
   },
     }
